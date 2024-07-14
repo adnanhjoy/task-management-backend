3c118d56a2024b9570e8e2c49d05a7415e8cc8e2
unknown <engr.adnanhossain@gmail.com>
2024-07-14 17:03:00 +0600

    complelete get and delete

--- src/controllers/teamMember/teamMember.controller.js
+++ src/controllers/teamMember/teamMember.controller.js
@@ -4,8 +4,11 @@ const { v4: uuidv4 } = require('uuid');
 //get all team members
 const getAllTeamMembers = async (req, res) => {
     try {
+        const allMembers = await pool.query(
+            "SELECT * FROM teammembers"
+        )
         res.status(200).json({
-            data: "hello Data"
+            data: allMembers.rows
         });
     } catch (error) {
         res.status(500).json({
@@ -36,7 +39,57 @@ const addTeamMembers = async (req, res) => {
     }
 };
 
+
+//delete team member
+
+const deleteTeamMembers = async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(id)
+        const deleteMembers = await pool.query(
+            'DELETE FROM teammembers WHERE id = $1 RETURNING *',
+            [id]
+        );
+
+        res.status(200).json({
+            message: 'Successfully deleted',
+            data: deleteMembers.rows
+        });
+    } catch (error) {
+        console.error("Error occurred:", error);
+        res.status(500).json({
+            error: "There was a server side error!",
+        });
+    }
+}
+
+
+
+// update team members 
+
+const updateTeamMembers = async (req, res) => {
+    // try {
+    //     const {id} = req.params;
+    //     const { name, email, password, role } = req.body;
+    //     const newMembers = await pool.query(
+    //         "INSERT INTO teammembers (id, name, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    //         [id, name, email, password, role]
+    //     );
+
+    //     res.status(200).json({
+    //         data: newMembers.rows
+    //     });
+    // } catch (error) {
+    //     res.status(500).json({
+    //         error: "There was a server side error"
+    //     })
+    // }
+}
+
+
 module.exports = {
     getAllTeamMembers,
-    addTeamMembers
+    addTeamMembers,
+    deleteTeamMembers,
+    updateTeamMembers
 }
--- src/controllers/teamMember/teamMember.route.js
+++ src/controllers/teamMember/teamMember.route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 //Get all members
 router.get('/', teamMemberController.getAllTeamMembers);
 router.post('/', teamMemberController.addTeamMembers);
+router.delete('/:id', teamMemberController.deleteTeamMembers);
 
 module.exports = { teamMemberRouter } = router
